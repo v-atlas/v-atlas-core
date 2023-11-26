8bde1cf3474508f8e7b31a069d224d9876b697c3
David Ibia <david.ibia@boxmarshall.com>
2023-11-26 05:35:48 +0100

    feat: added web5 session creation

--- stores/auth/index.ts
+++ stores/auth/index.ts
@@ -1,3 +1,12 @@
+import { Web5 } from "@web5/api";
 import { defineStore } from "pinia";
 
-export const useAuthStore = defineStore("auth", () => {});
+export const useAuthStore = defineStore("auth", () => {
+  async function startSession() {
+    const { web5, did: aliceDid } = await Web5.connect();
+  }
+
+  return {
+    startSession,
+  };
+});
