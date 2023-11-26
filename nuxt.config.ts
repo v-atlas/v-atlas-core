3cc1dd435a4e4886a16730e23d2d3a6edd9af42d
David Ibia <david.ibia@boxmarshall.com>
2023-11-26 05:25:03 +0100

    feat: added nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: [
+    "@nuxtjs/google-fonts",
+    [
+      "@pinia/nuxt",
+      {
+        disableVuex: true,
+        autoImports: ["acceptHMRUpdate"],
+      },
+    ],
+  ],
+  pinia: {
+    storesDirs: ["stores"],
+  },
+  $production: {
+    routeRules: {
+      "/**": {
+        ssr: true,
+      },
+    },
+  },
+  $development: {
+    devtools: { enabled: true },
+  },
+  devtools: { enabled: true },
+  googleFonts: {
+    display: "swap",
+    preconnect: true,
+    families: {
+      Jost: [300, 400, 500, 600, 700],
+    },
+  },
+  vite: {
+    server: {
+      hmr: {
+        protocol: "ws",
+        host: "0.0.0.0",
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "normalize.css";
+          @use "@/assets/scss/_colors.scss" as *;`,
+        },
+      },
+    },
+  },
+});
