eafe80fe6e30d1778bf73f0804c837aef88ce9dc
David Ibia <david.ibia@boxmarshall.com>
2023-11-26 23:01:35 +0100

    Add index.vue file with V-Atlas page content

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: "V-Atlas",
+  meta: [
+    {
+      key: "keywords",
+      content:
+        "V-Atlas, digital identity, Web5, virtual storage, data management, user privacy, decentralized platforms, identity security.",
+    },
+  ],
+});
+
+useSeoMeta({
+  title: "V-Atlas: Revolutionizing Digital Identity and Data Management",
+  description:
+    "Discover V-Atlas, a visionary platform using Web5 technology to empower users with secure, flexible virtual storage and identity management. Control your digital presence like never before.",
+});
+</script>
+
+<style lang="scss" scoped></style>
