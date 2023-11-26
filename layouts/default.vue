24fa746afc2d822565120c8ff23cb2e49aa00e0b
David Ibia <david.ibia@boxmarshall.com>
2023-11-26 05:28:23 +0100

    feat: creted default layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="v-atlas-app">
+    <AppHeader />
+    <div class="page">
+      <slot />
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+@import "~/assets/scss/default.scss";
+</style>
+
+<style lang="scss" scoped>
+div.v-atlas-app {
+  display: grid;
+  grid-template-rows: max-content auto max-content;
+  grid-template-columns: auto;
+  height: 100%;
+  div.page {
+    height: 100%;
+    min-height: calc(100vh - 100px);
+  }
+}
+</style>
