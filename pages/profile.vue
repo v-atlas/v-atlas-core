387b46d59cbb1464ae2fe87b4b0f164d982e22bf
David Ibia <david.ibia@boxmarshall.com>
2023-12-02 15:48:49 +0100

    Add profile page with Qrid component

---
+++ pages/profile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="profile">
+    <Qrid :did="myDid" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { $myDID: myDid } = useNuxtApp();
+</script>
+
+<style></style>
