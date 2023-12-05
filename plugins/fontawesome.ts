ad2256af56bbaaffa3fb08173f1382de5ad3d9b9
David Ibia <david.ibia@boxmarshall.com>
2023-12-05 19:57:25 +0100

    Add font awesome icons and plugin

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -7,10 +7,32 @@ import {
   faTimes,
   faRefresh,
   faQrcode,
+  faTimesCircle,
+  faCheckCircle,
 } from "@fortawesome/free-solid-svg-icons";
 
+import {
+  faSpotify,
+  faXTwitter,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
 library.add(
-  ...[faArrowRight, faCircleNotch, faTimes, faCopy, faRefresh, faQrcode]
+  ...[
+    faArrowRight,
+    faCircleNotch,
+    faTimes,
+    faCopy,
+    faRefresh,
+    faQrcode,
+    faSpotify,
+    faTimesCircle,
+    faCheckCircle,
+    faXTwitter,
+    faInstagram,
+    faLinkedin,
+  ]
 );
 
 // This is important, we are going to let Nuxt worry about the CSS
