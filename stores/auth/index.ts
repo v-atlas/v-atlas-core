10c3ecbe726e3fe048ddb65e7baa29fd5fd250cc
David Ibia <david.ibia@boxmarshall.com>
2023-12-05 20:28:00 +0100

    Refactor auth store to include Spotify access token

--- stores/auth/index.ts
+++ stores/auth/index.ts
@@ -1,12 +1,34 @@
-import { Web5 } from "@web5/api";
 import { defineStore } from "pinia";
 
-export const useAuthStore = defineStore("auth", () => {
-  async function startSession() {
-    const { web5, did: aliceDid } = await Web5.connect();
-  }
+export const useAuthStore = defineStore(
+  "auth",
+  () => {
+    const spotifyAccessToken = ref<string>("");
+    const isSpotifyConnected = computed<boolean>(() =>
+      Boolean(spotifyAccessToken.value && spotifyAccessToken.value.length > 0)
+    );
 
-  return {
-    startSession,
-  };
-});
+    function setSpotifyAccessToken(token: string) {
+      spotifyAccessToken.value = token;
+    }
+
+    function clearSpotifyAccessToken() {
+      spotifyAccessToken.value = "";
+    }
+
+    return {
+      spotifyAccessToken,
+      isSpotifyConnected,
+      setSpotifyAccessToken,
+      clearSpotifyAccessToken,
+    };
+  },
+  {
+    persist: {
+      paths: ["spotifyAccessToken"],
+      storage: persistedState.cookiesWithOptions({
+        sameSite: "strict",
+      }),
+    },
+  }
+);
