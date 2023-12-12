6d77cb7d52b7e6db653b8105665d4c071b5ad883
David Ibia <david.ibia@boxmarshall.com>
2023-12-12 04:22:04 +0100

    feat: moved shopify app

---
+++ pages/apps/spotify/manage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="landing">
+    <div class="content">
+      <div class="actions">
+        <link-spotify />
+      </div>
+
+      <div class="action-content">
+        <div class="spotify-actions" v-if="isSpotifyConnected">
+          <spotify-playlist-manager />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import { useAuthStore } from "~/stores/auth";
+
+const authStore = useAuthStore();
+const { isSpotifyConnected } = storeToRefs(authStore);
+</script>
+
+<style lang="scss" scoped>
+div.landing {
+  padding: 20px 20px;
+
+  @media screen and (min-width: 768px) {
+    padding: 20px 50px;
+  }
+
+  div.content {
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+    div.actions {
+      display: flex;
+      flex-direction: row;
+      gap: 20px;
+
+      // background-color: rgba($color: $primary-dark, $alpha: 0.2);
+      padding: 10px 5px;
+
+      width: max-content;
+    }
+
+    div.action-content {
+      padding: 10px;
+
+      // background-color: rgba($color: $primary-dark, $alpha: 0.2);
+
+      &:empty {
+        display: none;
+      }
+    }
+  }
+}
+</style>
