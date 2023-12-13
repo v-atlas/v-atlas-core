2757c03433f3ba7e5e2bd35011672091b29873cb
David Ibia <david.ibia@boxmarshall.com>
2023-12-13 13:08:00 +0100

    feat: import settings cog

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -14,6 +14,7 @@ import {
   faHome,
   faEye,
   faPlay,
+  faCog,
 } from "@fortawesome/free-solid-svg-icons";
 
 import {
@@ -42,6 +43,7 @@ library.add(
     faHome,
     faEye,
     faPlay,
+    faCog,
   ],
 );
 
