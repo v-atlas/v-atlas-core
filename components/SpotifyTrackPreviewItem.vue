287326cbd93c64a11625c41b4dfe93295a698bd5
David Ibia <david.ibia@boxmarshall.com>
2023-12-13 10:23:12 +0100

    feat: created track preview component

---
+++ components/SpotifyTrackPreviewItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="rounded-md bg-slate-500 p-2 text-sm">
+    <audio controls v-if="startPreview">
+      <source :src="mediaUrl" :type="sourceType" />
+    </audio>
+
+    <template v-else>
+      <button @click="togglePreview" class="w-full">
+        Listen to preview
+        <font-awesome-icon icon="fa-solid fa-play" class="ml-2" />
+      </button>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    mediaUrl: string;
+    sourceType?: string;
+  }>(),
+  {
+    sourceType: "audio/mpeg",
+  },
+);
+
+const startPreview = ref<boolean>(false);
+
+function togglePreview() {
+  startPreview.value = !startPreview.value;
+}
+</script>
+
+<style></style>
