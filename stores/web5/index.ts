2670b86bcd3cedebb805d76b7e18dff28e2ba7be
David Ibia <david.ibia@boxmarshall.com>
2023-12-13 18:40:09 +0100

    feat: base store implementation

--- stores/web5/index.ts
+++ stores/web5/index.ts
@@ -1,25 +1,73 @@
 import { defineStore } from "pinia";
 
+const RecordTypes = {
+  Text: "text/plain",
+  Image: "image/png",
+  File: "application/octet-stream",
+  Json: "application/json",
+} as const;
+
+type RecordType = (typeof RecordTypes)[keyof typeof RecordTypes];
+
+type RecordOptions =
+  | {
+    data: string | Blob;
+    schema?: string;
+    type: RecordType;
+  }
+  | {
+    type: "text/plain";
+    data: string;
+    schema: undefined;
+  }
+  | {
+    type: "image/png";
+    data: Blob;
+    schema: undefined;
+  }
+  | {
+    type: "application/octet-stream";
+    data: Blob;
+    schema: string;
+  }
+  | {
+    type: "application/json";
+    schema?: string;
+    data: string;
+  };
+
 export const useWeb5Store = defineStore("web5", () => {
   const { $myDID: did, $web5: web5Instance } = useNuxtApp();
 
-  async function fetchData() {}
+  async function fetchRecord() { }
 
-  async function queryData() {}
+  async function queryRecord() { }
 
-  async function setData() {}
+  async function createRecord(options: RecordOptions) {
 
-  async function deleteData() {}
+    const { record } = await web5Instance.dwn.records.create({
+      data: options.data,
+      message: {
+        dataFormat: options.type,
+        schema: options.schema,
+      },
+    });
 
-  async function grantAccess() {}
+    console.log(record);
+  }
 
-  async function revokeAccess() {}
+  async function deleteRecord() { }
+
+  async function grantRecordAccess() { }
+
+  async function revokeRecordAccess() { }
 
   return {
-    fetchData,
-    queryData,
-    setData,
-    deleteData,
-    revokeAccess,
+    fetchRecord,
+    queryRecord,
+    createRecord,
+    deleteRecord,
+    grantRecordAccess,
+    revokeRecordAccess,
   };
 });
