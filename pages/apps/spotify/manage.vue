6e9e4ace3d21183d0c0ef5277661ccb87d69fc11
David Ibia <david.ibia@boxmarshall.com>
2023-12-13 21:47:47 +0100

    feat: updated page layout

--- pages/apps/spotify/manage.vue
+++ pages/apps/spotify/manage.vue
@@ -1,16 +1,6 @@
 <template>
-  <div class="landing">
-    <div class="content">
-      <div class="actions">
-        <link-spotify />
-      </div>
-
-      <div class="action-content">
-        <div class="spotify-actions" v-if="isSpotifyConnected">
-          <spotify-playlist-manager />
-        </div>
-      </div>
-    </div>
+  <div class="flex flex-col gap-10">
+    <spotify-playlist-manager v-if="isSpotifyConnected" />
   </div>
 </template>
 
@@ -40,38 +30,4 @@ const authStore = useAuthStore();
 const { isSpotifyConnected } = storeToRefs(authStore);
 </script>
 
-<style lang="scss" scoped>
-div.landing {
-  padding: 20px 20px;
-
-  @media screen and (min-width: 768px) {
-    padding: 20px 50px;
-  }
-
-  div.content {
-    display: flex;
-    flex-direction: column;
-    gap: 50px;
-    div.actions {
-      display: flex;
-      flex-direction: row;
-      gap: 20px;
-
-      // background-color: rgba($color: $primary-dark, $alpha: 0.2);
-      padding: 10px 5px;
-
-      width: max-content;
-    }
-
-    div.action-content {
-      padding: 10px;
-
-      // background-color: rgba($color: $primary-dark, $alpha: 0.2);
-
-      &:empty {
-        display: none;
-      }
-    }
-  }
-}
-</style>
+<style scoped></style>
