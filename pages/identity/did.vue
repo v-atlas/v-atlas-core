c36b125df94f0b7bbccf0c9b62f0b6c4e12a7348
David Ibia <david.ibia@boxmarshall.com>
2023-12-15 13:06:17 +0100

    feat: render DID

--- pages/identity/did.vue
+++ pages/identity/did.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="mb-10">
-    <div class="mt-8 mb-6 flex gap-[10px]">
-      <div class="p-[7px] rounded-[100px] bg-[#1b1f31]">  <img
-            src="~/assets/images/previous.svg"
-            alt="previous icon"
-          /></div>
-          <div class="p-[7px] rounded-[100px] bg-[#1b1f31]">
-            <img
-            src="~/assets/images/next.svg"
-            alt="next icon"
-          />
-          </div>
+    <div class="mb-6 mt-8 flex gap-[10px]">
+      <div class="rounded-[100px] bg-[#1b1f31] p-[7px]">
+        <img src="~/assets/images/previous.svg" alt="previous icon" />
+      </div>
+      <div class="rounded-[100px] bg-[#1b1f31] p-[7px]">
+        <img src="~/assets/images/next.svg" alt="next icon" />
+      </div>
     </div>
     <div
       class="relative mx-auto flex w-[90%] max-w-[624px] flex-col items-center overflow-hidden rounded-[10px] border-[1.4px] border-[#DCC23C] bg-primary px-5 pb-10 pt-12 text-center lg:pb-[132px]"
@@ -26,14 +22,50 @@
         alt="info icon"
         class="relative mb-[49px]"
       />
-      <p class="relative text-xs leading-8 text-subdued">DID NUMBER</p>
+      <p class="relative text-xs leading-8 text-subdued">DID</p>
       <div
         class="mb-[49px] flex gap-[17px] rounded-[100px] bg-[#1e2235] px-5 py-[10px]"
       >
-        <p>485869604005</p>
-        <img src="~/assets/images/copy-white.svg" alt="copy icon" />
+        <p class="max-w-sm truncate">{{ did }}</p>
+        <button @click="handleCopyDID">
+          <img src="~/assets/images/copy-white.svg" alt="copy icon" />
+        </button>
       </div>
-      <img src="~/assets/images/qr-code.svg" alt="qrcode icon" />
+      <canvas ref="qrCanvas" class="scale-75"></canvas>
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import qrcode from "qrcode";
+import { useClipboard } from "@vueuse/core";
+const { copy } = useClipboard();
+
+const { $myDID: did } = useNuxtApp();
+
+const qrCanvas = ref<HTMLCanvasElement | null>(null);
+
+function handleCopyDID() {
+  copy(did);
+}
+
+function generateQRCode() {
+  if (!qrCanvas.value) {
+    return;
+  }
+
+  if (!did) {
+    return;
+  }
+
+  qrcode.toCanvas(qrCanvas.value, did, {
+    margin: 1,
+  });
+}
+
+watch(() => did, generateQRCode);
+
+onMounted(() => {
+  generateQRCode();
+});
+</script>
