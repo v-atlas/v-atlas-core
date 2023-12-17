67670ab72f436d8a50fd16af96d9b2cbcc625b58
David Ibia <david.ibia@boxmarshall.com>
2023-12-17 16:02:59 +0100

    feat: updated oauth integration

--- server/api/google-drive-fetch-files.ts
+++ server/api/google-drive-fetch-files.ts
@@ -1,36 +1,50 @@
 import { google } from "googleapis";
 import { z } from "zod";
 
-const oauthCredentialSchema = z.object({
+const googleDriveAuthorizationSchema = z.object({
   accessToken: z.string(),
 });
 
-type OauthCredentialSchema = z.infer<typeof oauthCredentialSchema>;
+type GoogleDriveAuthorizationOptions = z.infer<typeof googleDriveAuthorizationSchema>;
 
 export default defineEventHandler(async (event) => {
   try {
-    const { accessToken } = await readBody<OauthCredentialSchema>(event);
-    const authClient = google.auth.fromAPIKey(accessToken);
+
+    const { googleCloudClientId, googleCloudClientSecret, googleCloudRedirectUrl } =
+      useRuntimeConfig();
+
+    const rawBody = await readBody<GoogleDriveAuthorizationOptions>(event);
+    const { accessToken } = googleDriveAuthorizationSchema.parse(rawBody)
+
+    const authClient = new google.auth.OAuth2(googleCloudClientId, googleCloudClientSecret, googleCloudRedirectUrl);
+
+
+    authClient.setCredentials({
+      refresh_token: accessToken
+    })
+
 
     const drive = google.drive({ version: "v3", auth: authClient });
 
-    const res = await drive.files.list({
+    const { data } = await drive.files.list({
       pageSize: 10,
       fields: "nextPageToken, files(id, name)",
     });
 
-    const files = res.data.files;
+
     return {
       success: true,
-      message: "Successfully generated authorization url",
-      files,
+      message: "Successfully fetched files from Google Drive",
+      data,
     };
   } catch (error) {
+
+
     if (error instanceof z.ZodError) {
       return {
         success: false,
         message: error.issues.join(", "),
-        files: null,
+        data: null,
       };
     }
 
@@ -38,13 +52,13 @@ export default defineEventHandler(async (event) => {
       return {
         success: false,
         message: error.message,
-        files: null,
+        data: null,
       };
     }
     return {
       success: false,
-      message: "Failed to fetch drive files",
-      files: null,
+      message: "Failed to fetch Google Drive files",
+      data: null,
     };
   }
 });
